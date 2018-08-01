0ea152b7385467a07af0741bf190b9099946e778
haoja <yezichun@gmail.com>
2018-07-31 20:40:14 -0700

    done assignment 1

--- assignment/a1/example_outliers.jl
+++ assignment/a1/example_outliers.jl
@@ -5,16 +5,16 @@ data = load("outliersData.jld")
 
 # Fit a least absolutes model
 include("leastSquares.jl")
-model = lesatAbsolutes(X,y)
+model = lesatmax(X,y)
 
 # Evaluate training error
 yhat = model.predict(X)
-trainError = mean(abs(yhat - y))
+trainError = maximum(abs.(yhat - y))
 @printf("Squared train Error with least absolutes: %.3f\n",trainError)
 
 # Evaluate test error
 yhat = model.predict(Xtest)
-testError = mean(abs(yhat - ytest))
+testError = maximum(abs.(yhat - ytest))
 @printf("Squared test Error with least absolutes: %.3f\n",testError)
 
 # Plot model
--- assignment/a1/leastSquares.jl
+++ assignment/a1/leastSquares.jl
@@ -22,17 +22,42 @@ function lesatAbsolutes(X, y)
 	(n, d) = size(X)
 	Z = [ones(n,1) X]
 
-    c = zeros(d+1+n,1)
-	c[d+1:end] = 1
+    c = zeros(d+1+n)
+	c[d+2:end] = 1.0
 
 	A = [-Z eye(n);Z eye(n)]
-	d = [-y[:];y[:]]
-	b = fill(Inf, d+1+n)
+	da = [-y[:];y[:]]
+	b = fill(Inf, 2*n)
 
     lb = fill(-Inf, d+1+n)
 	ub = fill(Inf, d+1+n)
 
-	solution = linprog(c,A,d,b,lb,ub,GLPKSolverLP())
+	solution = linprog(c,A, da,b,lb,ub,GLPKSolverLP())
+	w = solution.sol[1:d+1]
+
+	predict(Xtilde) = [ones(size(Xtilde,1),1) Xtilde]*w
+
+    return LinearModel(predict,w)
+
+end
+
+
+function lesatmax(X, y)
+
+	(n, d) = size(X)
+	Z = [ones(n,1) X]
+
+    c = zeros(d+1+1)
+	c[d+2:end] = 1.0
+
+	A = [-Z ones(n,1);Z ones(n,1)]
+	da = [-y[:];y[:]]
+	b = fill(Inf, 2*n)
+
+    lb = fill(-Inf, d+1+1)
+	ub = fill(Inf, d+1+1)
+
+	solution = linprog(c,A, da,b,lb,ub,GLPKSolverLP())
 	w = solution.sol[1:d+1]
 
 	predict(Xtilde) = [ones(size(Xtilde,1),1) Xtilde]*w
